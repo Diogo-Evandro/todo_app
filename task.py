9c4878ea734adf40317eec346dcfbb07dfbb3de3
Diogo <evandrodiogo425@gmail.com>
2024-05-30 17:13:24 -0100

    remove-task commit

--- task.py
+++ task.py
@@ -10,15 +10,25 @@ def list_tasks():
         print(f"{i + 1}. {task['task']} [{status}]")
 
 
+def remove_task(index):
+    if 0 >= index < len(tasks):
+        tasks.pop(index)
+    else:
+        print("Índice inválido")
+
+
 def main():
     print("Personal Task Manager")
     while True:
-        command = input("Comando (add/list/exit): ")
+        command = input("Comando (add/list/remove/exit): ")
         if command == "add":
             task = input("Tarefa: ")
             add_task(task)
         elif command == "list":
             list_tasks()
+        elif command == "remove":
+            index = int(input("Índice da tarefa a remover: ")) - 1
+            remove_task(index)
         elif command == "exit":
             break
 
